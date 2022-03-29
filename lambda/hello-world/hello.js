6e2528a98bad19dbd5e9672103bf6f1f603a1ac6
Petr Stukalov <petr.marketlab@gmail.com>
2022-03-30 00:07:21 +0300

    test

--- lambda/hello-world/hello.js
+++ lambda/hello-world/hello.js
@@ -2,9 +2,15 @@
 
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
-  let responseMessage = 'Hello, World!';
+  let responseMessage = `Welcome to our demo API, here are the details of your request:
+  Headers: ${JSON.stringify(event.headers)}
+  Method: ${event.httpMethod}`;
 
-  return `Welcome to our demo API, here are the details of your request:
-Headers: JSON.stringify(event.headers)
-Method: Get Body: {"username":"xyz", "password":"xyz"}`
+  return {
+    statusCode: 200,
+    headers: event.headers,
+    body: JSON.stringify({
+      message: responseMessage,
+    }),
+  }
 }
